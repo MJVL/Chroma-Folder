79eca7a48ff1ad705116b657004e51d53b00f33c
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2020-01-03 12:37:23 -0500

    Refactor GUI

--- chroma-folder-gui.py
+++ chroma-folder-gui.py
@@ -1,69 +1,83 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QInputDialog, QLineEdit, QFileDialog
 import sys
 import os
 
-
-class Ui_MainWindow(object):
+class ChromaGUI(QMainWindow):
 
     def __init__(self):
+        super(ChromaGUI, self).__init__()
         self.filename = None
-
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(241, 129)
-        MainWindow.setAutoFillBackground(False)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.btnChange = QtWidgets.QPushButton(self.centralwidget)
-        self.btnChange.setGeometry(QtCore.QRect(10, 90, 221, 31))
+        self.savename = None
+        self.initUI()
+    
+    def initUI(self):
+        self.resize(241, 129)
+        self.setWindowTitle("Chroma Folder")
+        
+        self.btnChange = QtWidgets.QPushButton(self)
+        self.btnChange.setGeometry(QtCore.QRect(5, 90, 160, 31))
+        self.btnChange.setText("Change Folder Icons")
         self.btnChange.setObjectName("btnChange")
-        self.lblDir = QtWidgets.QLabel(self.centralwidget)
+
+        self.btnReset = QtWidgets.QPushButton(self)
+        self.btnReset.setGeometry(QtCore.QRect(155, 90, 81, 31))
+        self.btnReset.setText("Reset")
+        self.btnReset.setObjectName("btnClear")
+
+        self.lblDir = QtWidgets.QLabel(self)
         self.lblDir.setGeometry(QtCore.QRect(10, 10, 71, 16))
+        self.lblDir.setText("Directory:")
         self.lblDir.setObjectName("lblDir")
-        self.txtDir = QtWidgets.QLineEdit(self.centralwidget)
+
+        self.txtDir = QtWidgets.QLineEdit(self)
         self.txtDir.setGeometry(QtCore.QRect(90, 8, 141, 20))
+        self.txtDir.setText(os.path.expanduser("~/Desktop"))
         self.txtDir.setObjectName("txtDir")
-        self.horTop = QtWidgets.QFrame(self.centralwidget)
+
+        self.horTop = QtWidgets.QFrame(self)
         self.horTop.setGeometry(QtCore.QRect(10, 30, 221, 20))
         self.horTop.setFrameShape(QtWidgets.QFrame.HLine)
         self.horTop.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.horTop.setObjectName("horTop")
-        self.chkLoad = QtWidgets.QCheckBox(self.centralwidget)
+
+        self.chkLoad = QtWidgets.QCheckBox(self)
         self.chkLoad.setGeometry(QtCore.QRect(10, 50, 101, 17))
+        self.chkLoad.setText("Load Config")
         self.chkLoad.setObjectName("chkLoad")
-        self.horBot = QtWidgets.QFrame(self.centralwidget)
+
+        self.horBot = QtWidgets.QFrame(self)
         self.horBot.setGeometry(QtCore.QRect(10, 70, 221, 20))
         self.horBot.setFrameShape(QtWidgets.QFrame.HLine)
         self.horBot.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.horBot.setObjectName("horBot")
-        self.chkSave = QtWidgets.QCheckBox(self.centralwidget)
+
+        self.chkSave = QtWidgets.QCheckBox(self)
         self.chkSave.setGeometry(QtCore.QRect(130, 50, 101, 17))
+        self.chkSave.setText("Save Config")
         self.chkSave.setObjectName("chkSave")
-        MainWindow.setCentralWidget(self.centralwidget)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-        self.txtDir.setText(os.path.expanduser("~/Desktop"))
         self.btnChange.clicked.connect(self.change)
+        self.btnReset.clicked.connect(self.reset)
         self.chkLoad.stateChanged.connect(self.loadConfig)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Chroma Folder"))
-        self.btnChange.setText(_translate("MainWindow", "Change Folder Icons"))
-        self.lblDir.setText(_translate("MainWindow", "Directory:"))
-        self.chkLoad.setText(_translate("MainWindow", "Load Config"))
-        self.chkSave.setText(_translate("MainWindow", "Save Config"))
+        self.chkSave.stateChanged.connect(self.saveConfig)
+        
 
     def change(self):
         pass
 
+
+    def reset(self):
+        self.txtDir.setText(os.path.expanduser("~/Desktop"))
+        self.chkLoad.setChecked(False)
+        self.chkSave.setChecked(False)
+        self.filename = self.savename = None
+        self.repaint()
+
+
     def loadConfig(self):
         if self.chkLoad.isChecked():
-            self.filename = QtWidgets.QFileDialog.getOpenFileName(None, "Select Config", "data", "Icon Files (*.icns)")
+            self.filename = QFileDialog.getOpenFileName(None, "Select Config", "data", "Icon Files (*.icns)")
             if self.filename:
                 msgBox = QMessageBox()
                 msgBox.setIcon(QMessageBox.Information)
@@ -78,16 +92,22 @@ class Ui_MainWindow(object):
                 msgBox.setWindowTitle("Config Load Fail")
                 msgBox.setStandardButtons(QMessageBox.Ok)
                 returnValue = msgBox.exec()
+        else:
+            self.filename = None
+
+
+    def saveConfig(self):
+        if self.chkSave.isChecked():
+            self.saveConfig = QInputDialog.getText(self, "Save Config","Config name:", QLineEdit.Normal, "")[0].split(".")[0] + ".icns"
+        else:
+            self.savename = None
 
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    app = QApplication(sys.argv)
+    win = ChromaGUI()
+    win.show()
     sys.exit(app.exec_())
-
 
 if __name__ == "__main__":
     main()
